e88259d9ba63b03c0420aa88a627368ade3c487f
lzxb <1340641314@qq.com>
2022-08-21 01:41:47 +0800

    test: feat

--- tests/set-value.spec.ts
+++ tests/set-value.spec.ts
@@ -5,7 +5,21 @@ import SetValue from './set-value.vue';
 
 test('Base', async () => {
     const wrapper = mount(SetValue);
-    assert.equal(wrapper.find('p').text(), '0');
-    await wrapper.find('button').trigger('click');
-    assert.equal(wrapper.find('p').text(), '1');
+    assert.equal(wrapper.find('.value').text(), '0');
+    assert.equal(wrapper.find('.count').text(), '0');
+
+    await wrapper.find('.value-btn').trigger('click');
+    
+    assert.equal(wrapper.find('.value').text(), '1');
+    assert.equal(wrapper.find('.count').text(), '0');
+
+    await wrapper.find('.count-btn').trigger('click');
+    assert.equal(wrapper.find('.value').text(), '1');
+    assert.equal(wrapper.find('.count').text(), '100');
+
+    await wrapper.find('.count-btn2').trigger('click');
+    assert.equal(wrapper.find('.value').text(), '1');
+    assert.equal(wrapper.find('.count').text(), '49');
+
+
 });
--- tests/set-value.vue
+++ tests/set-value.vue
@@ -3,16 +3,34 @@ import { defineComponent } from 'vue';
 import { Setup, Context } from 'vue-class-setup';
 
 @Setup
-class App extends Context {
+class Base extends Context {
+    public count = 0;
+    public setCount(count: number) {
+        this.count = count;
+    }
+    public resetCount() {
+        return (count: number) => {
+            this.count = --count;
+        }
+    }
+}
+
+@Setup
+class App extends Base {
     public value = 0;
 }
 export default defineComponent({
     ...App.inject()
 })
 </script>
+<script lang="ts" setup>
+</script>
 <template>
     <div>
-        <p>{{ value }}</p>
-        <button @click="value++"></button>
+        <p class="value">{{ value }}</p>
+        <p class="count">{{ count }}</p>
+        <button class="value-btn" @click="value++"></button>
+        <button class="count-btn" @click="setCount(100)"></button>
+        <button class="count-btn2" @click="setCount = resetCount(); setCount(50)"></button>
     </div>
 </template>
