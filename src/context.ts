7628ae6020225263e990baf71877e987765c1f58
lzxb <1340641314@qq.com>
2022-08-24 21:02:53 +0800

    feat: WHITE_LIST

--- src/context.ts
+++ src/context.ts
@@ -27,6 +27,8 @@ export function setCurrentHookName(name: TargetName | null) {
     currentName = name;
 }
 
+const WHITE_LIST: string[] = [SETUP_SETUP_DEFINE, SETUP_SETUP_DEFINE, '$vm', '$emit', '$props'];
+
 function use(target: VueInstance, _This: any) {
     let use: Map<any, InstanceType<DefineConstructor>>;
     if (target[SETUP_USE]) {
@@ -50,7 +52,7 @@ function use(target: VueInstance, _This: any) {
         PropertyDescriptor
     >;
     names.forEach((name) => {
-        if (propertyDescriptor.has(name) || name === SETUP_SETUP_DEFINE) return;
+        if (propertyDescriptor.has(name) || WHITE_LIST.includes(name)) return;
         defineProperty(name, {
             get() {
                 return app[name];
@@ -61,6 +63,7 @@ function use(target: VueInstance, _This: any) {
         });
     });
     propertyDescriptor.forEach((value, name) => {
+        if (WHITE_LIST.includes(name)) return;
         defineProperty(name, {
             get() {
                 return app[name];
